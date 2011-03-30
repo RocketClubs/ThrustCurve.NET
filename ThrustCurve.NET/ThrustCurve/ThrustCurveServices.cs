9365792ef25e2d2d433d460313fc39005f5befd7
Nicholas Larsen <fodylarsen@gmail.com>
2011-03-29 23:04:47 -0400

    initial checkin, decided against going with data contracts because they do not support xml attributes and I have no control over the webservice source

---
+++ ThrustCurve.NET/ThrustCurve/ThrustCurveServices.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace RocketClubs.ThrustCurve
+{
+    public class ThrustCurveServices
+    {
+        private const string SEARCH_API_URL = "http://www.thrustcurve.org/servlets/search";
+        private const string DOWNLOAD_API_URL = "http://www.thrustcurve.org/servlets/download";
+        private const string METADATA_API_URL = "http://www.thrustcurve.org/servlets/metadata";
+
+        public SearchResults Search(SearchParameters parameters)
+        {
+            return MakeServiceRequest<SearchResults>(SEARCH_API_URL, parameters);
+        }
+
+        public MetadataResults Metadata(MetadataParameters parameters)
+        {
+            return MakeServiceRequest<MetadataResults>(METADATA_API_URL, parameters);
+        }
+
+        public DownloadResults Metadata(DownloadParameters parameters)
+        {
+            return MakeServiceRequest<DownloadResults>(DOWNLOAD_API_URL, parameters);
+        }
+
+        private T MakeServiceRequest<T>(string serviceUrl, object parameters)
+        {
+            T results = default(T);
+
+            HttpWebRequest serviceRequest = WebRequest.Create(serviceUrl) as HttpWebRequest;
+            serviceRequest.Method = "POST";
+
+            var xmlSerializer = new XmlSerializer(parameters.GetType());
+            xmlSerializer.Serialize(serviceRequest.GetRequestStream(), parameters);
+
+           
+            HttpWebResponse serviceResponse = serviceRequest.GetResponse() as HttpWebResponse;
+            if (serviceResponse.StatusCode == HttpStatusCode.OK)
+            {
+                var responseSerializer = new XmlSerializer(typeof(T));
+                results = (T)responseSerializer.Deserialize(serviceResponse.GetResponseStream());
+            }
+
+            return results;
+        }
+    }
+}
